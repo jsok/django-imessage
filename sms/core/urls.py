f339c74809589507fc6ca99423b76282cda08b26
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2012-10-05 15:16:27 +1000

    Initial commit of iMessage database project.

---
+++ sms/core/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sms.views.home', name='home'),
+    # url(r'^sms/', include('sms.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
